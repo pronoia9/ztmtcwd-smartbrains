9da9f3240ba4247ffe9d1bcc612948243a1b7ee0
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-13 08:18:26 +0300

    import preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
+import Preloader from './components/Preloader/Preloader';
 
 export default function App() {
-  return <div className='App'></div>;
+  return <div className='App'><Preloader /></div>;
 }

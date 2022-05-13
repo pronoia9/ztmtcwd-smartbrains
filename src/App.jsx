24ed389b684cb0b196046a5339d0b554c2205e55
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-13 17:27:16 +0300

    move app.jsx and fix imports

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './App.css';
-import Preloader from './components/Preloader/Preloader';
-
-export default function App() {
-  return <div className='App'><Preloader /></div>;
-}
---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Preloader from './Preloader/Preloader';
+
+export default function App() {
+  return (
+    <div className='App'>
+      <Preloader />
+    </div>
+  );
+}

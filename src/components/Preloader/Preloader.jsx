aab3779f2b08a272f69f0e098a8ff36d05f717b9
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-13 16:56:02 +0300

    add pace.js

---
+++ src/components/Preloader/Preloader.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect } from 'react';
+import Pace from './Pace.js';
+import './Preloader.css';
+
+export default function Preloader() {
+  useEffect(() => {
+    Pace();
+  });
+  return (
+    <div id='preload-ing'>
+      <div className='showX'>
+        <div className='loading'>
+          <span>L</span>
+          <span>o</span>
+          <span>a</span>
+          <span>d</span>
+          <span>i</span>
+          <span>n</span>
+          <span>g</span>
+        </div>
+        <div id='preloader' className=''></div>
+      </div>
+    </div>
+  );
+}
+
+const PaceStart = () => {
+  document.querySelector('#preloader').classList.remove('isdone');
+  document.querySelector('.loading').classList.remove('isdone');
+};
+
+const PaceDone = () => {
+  document.querySelector('#preloader').classList.add('isdone');
+  document.querySelector('.loading').classList.add('isdone');
+};
+const PaceBig = () => {
+  if (document.querySelector('body').classList.contains('pace-done')) {
+    document.querySelector('#preloader').classList.add('isdone');
+    document.querySelector('.loading').classList.add('isdone');
+
+    window.addEventListener('load', function() {
+      document.querySelector('#preloader').classList.add('isdone');
+      document.querySelector('.loading').classList.add('isdone');
+      document.querySelector('.pace-running.pace-running') &&
+        document.querySelector('.pace-running.pace-running').classList.remove('pace-running');
+    });
+  }
+};
+
+// f = function () {
+//   Pace.on('start', function () {
+//     document.querySelector('#preloader').classList.remove('isdone'), document.querySelector('.loading').classList.remove('isdone');
+//   }),
+//     Pace.on('done', function () {
+//       document.querySelector('#preloader').classList.add('isdone'), document.querySelector('.loading').classList.add('isdone');
+//     }),
+//     document.querySelector('body').classList.contains('pace-done') &&
+//       (document.querySelector('#preloader').classList.add('isdone'), document.querySelector('.loading').classList.add('isdone')),
+//     window.addEventListener('load', function () {
+//       document.querySelector('#preloader').classList.add('isdone'),
+//         document.querySelector('.loading').classList.add('isdone'),
+//         document.querySelector('.pace-running.pace-running') &&
+//           document.querySelector('.pace-running.pace-running').classList.remove('pace-running');
+//     });
+// };

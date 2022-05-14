282459b0c0898d408144c993bc8c97777903b5b0
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-14 05:40:52 +0300

    import navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Preloader from './Preloader/Preloader';
+import Navbar from './Navbar/Navbar';
 
 export default function App() {
   return (
     <div className='App'>
       <Preloader />
+      <Navbar />
     </div>
   );
 }

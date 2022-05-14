134cae4be367dffb9ca45bfe80e0ef26975c1e91
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-14 05:31:41 +0300

    add ids

--- src/components/general/Div.jsx
+++ src/components/general/Div.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
-export default function Div({ children, classNames, index = 0 }) {
-  return <div className={classNames[index]}>{index < classNames.length - 1 ? <Div classNames={classNames} index={index + 1}>{children}</Div> : children}</div>;
+export default function Div({ properties, children, classNames, ids, index = 0 }) {
+  // console.log('properties[', index, ']:', properties[index])
+  // console.log(Object.keys(properties[index]), '=', Object.values(properties[index]));
+
+  return (
+    <div className={classNames[index]} id={ids[index]}>
+      {(index < properties.length - 1) ? <Div properties={properties} classNames={classNames} ids={ids} index={index + 1}>{children}</Div> : children}
+    </div>
+  );
 }

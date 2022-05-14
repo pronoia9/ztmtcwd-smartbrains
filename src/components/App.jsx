c86cd0c8b46ecfa08161963a9abeca3080c4b905
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-14 05:45:49 +0300

    change classname to __next

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import Navbar from './Navbar/Navbar';
 
 export default function App() {
   return (
-    <div className='App'>
+    <div className='__next'>
       <Preloader />
       <Navbar />
     </div>

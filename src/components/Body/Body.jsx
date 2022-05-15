777b9267ea5ba247fdec5c62613a697faed74fa7
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-15 16:44:38 +0300

    separate component for logged in or not

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -3,33 +3,18 @@ import './Body.css';
 import Div from '../General/Div';
 import ImageLinkForm from '../ImageLinkForm/ImageLinkForm';
 
-export default function Header() {
-  let status = false; // temporary check
-
+export default function Body() {
   return (
-    <div className='container'>
+    <Div classNames={['container']}>
       <Div classNames={['page-header', 'container', 'row justify-content-center', 'col-lg-10 col-md-8', 'cont text-center']}>
-        {status ? (
-          // logged in
-          <>
-            <h2 className='mb-10'>
-              <span className='color-font'>Andrei</span> you are ranked <span className='color-font'>#5</span>
-            </h2>
-            <ImageLinkForm />
-          </>
-        ) : (
-          // not logged in
-          <h1 className='mb-10'>
-            <span className='color-font'>SmartBrains</span> will detect faces in your <span className='color-font'>pictures</span>.
-          </h1>
-        )}
+        <h2 className='mb-10'>
+          <span className='color-font'>Andrei</span> you are ranked <span className='color-font'>#5</span>
+        </h2>
+        <ImageLinkForm />
       </Div>
-      {status && (
-        <Div
-          classNames={['blog-pg single section-padding pt-0', 'container', 'row justify-content-center', 'col-lg-11', 'post', 'img']}>
-          <img src='https://www.goodworklabs.com/wp-content/uploads/2020/06/Face-recognition-technology.png' alt='' />
-        </Div>
-      )}
-    </div>
+      <Div classNames={['blog-pg single section-padding pt-0', 'container', 'row justify-content-center', 'col-lg-11', 'post', 'img']}>
+        <img src='https://www.goodworklabs.com/wp-content/uploads/2020/06/Face-recognition-technology.png' alt='' />
+      </Div>
+    </Div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,13 @@
-import react from "react";
+import react from 'react';
+import Div from '../General/Div';
+import './Header.css';
 
 export default function Header() {
-  return <></>
+  return (
+    <Div classNames={['page-header', 'container', 'row justify-content-center', 'col-lg-10 col-md-8', 'cont text-center']}>
+      <h1 className='mb-10'>
+        <span className='color-font'>SmartBrains</span> will detect faces in your <span className='color-font'>pictures</span>.
+      </h1>
+    </Div>
+  );
 }

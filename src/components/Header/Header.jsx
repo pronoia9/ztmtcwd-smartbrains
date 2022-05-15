0495f6492cb6fc0c1833bbf1d7580beafbe2bf26
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-15 16:49:58 +0300

    change header text/container classes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,8 @@ import './Header.css';
 
 export default function Header() {
   return (
-    <Div classNames={['page-header', 'container', 'row justify-content-center', 'col-lg-10 col-md-8', 'cont text-center']}>
-      <h1 className='mb-10'>
+    <Div classNames={['container', 'row justify-content-center', 'col-lg-10', 'cont text-center']}>
+      <h1>
         <span className='color-font'>SmartBrains</span> will detect faces in your <span className='color-font'>pictures</span>.
       </h1>
     </Div>

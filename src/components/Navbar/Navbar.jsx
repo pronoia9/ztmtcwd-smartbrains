a624647464c09f38edc057948f9f51db630b08c3
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-15 16:33:22 +0300

    remove tilt

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+// import Tilt from 'react-tilt';
 import './Navbar.css';
 
 export default function Navbar({ logo }) {
   return (
     <nav className='navbar navbar-expand-lg change'>
       <div className='container'>
-        <a className='logo' href='/'><img src={logo} alt='logo' /></a>
+        {/* <Tilt className="Tilt" options={{ max: 25 }}> */}
+          <a className='logo' href='/'><img src={logo} alt='logo' /></a>
+        {/* </Tilt> */}
         {/* <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
           <span className='icon-bar'><i className='fas fa-bars'></i></span>
         </button> */}

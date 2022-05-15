442a08bc8426c7fe6874b6ee267e2b46788bda43
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-15 16:34:18 +0300

    change check for further classnames/ids

--- src/components/general/Div.jsx
+++ src/components/general/Div.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
-export default function Div({ properties, children, classNames, ids, index = 0 }) {
+export default function Div({ children, properties = [], classNames = [], ids = [], index = 0 }) {
   // console.log('properties[', index, ']:', properties[index])
   // console.log(Object.keys(properties[index]), '=', Object.values(properties[index]));
 
   return (
+    // <div className={classNames[index]} id={ids[index]}>
+    //   {(index < properties.length - 1) ? <Div properties={properties} classNames={classNames} ids={ids} index={index + 1}>{children}</Div> : children}
+    // </div>
     <div className={classNames[index]} id={ids[index]}>
-      {(index < properties.length - 1) ? <Div properties={properties} classNames={classNames} ids={ids} index={index + 1}>{children}</Div> : children}
+      {index < classNames.length - 1 || index < ids.length - 1 ? (
+        <Div classNames={classNames} ids={ids} index={index + 1}>
+          {children}
+        </Div>
+      ) : (
+        children
+      )}
     </div>
   );
 }

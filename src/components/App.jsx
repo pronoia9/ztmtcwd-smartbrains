b138df2b2f55895360714d2be62755e98f78617b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-15 16:32:51 +0300

    add particles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
 // styles
 import './App.css';
 // components
 import Preloader from './Preloader/Preloader';
 import Navbar from './Navbar/Navbar';
-// other
-const logo = require('../images/logo6.png');
+import Header from './Body/Body';
+// other / data
+import particlesOptions from '../data/particles.json';
+const logo = require('../images/logo.png');
 
 export default function App() {
+  const particlesInit = useCallback((main) => {
+    loadFull(main);
+  }, []);
+
   return (
     <div className='__next'>
       <Preloader />
       <Navbar logo={logo} />
+
+      <div className='particles circle-bg valign'>
+        <Header />
+
+        <div className='gradient-circles'>
+          <Particles options={particlesOptions} init={particlesInit}/>
+          <div></div>
+          <div className='gradient-circle'></div>
+          <div className='gradient-circle two'></div>
+          <div className='line bottom left'></div>
+        </div>
+      </div>
     </div>
   );
 }

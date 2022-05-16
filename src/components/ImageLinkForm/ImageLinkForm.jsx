4154f1fdcda348f202836bebe903eca6d44858b3
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-16 14:41:16 +0300

    change placeholder to props.text

--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import Div from '../General/Div';
 import './ImageLinkForm.css';
 
-export default function ImageLinkForm({text}) {
+export default function ImageLinkForm({ text = 'Placeholder' }) {
   return (
     <Div id={['image-link-form']} classNames={['image-link']}>
-      <input type='text' placeholder='Placeholder' />
+      <input type='text' placeholder={text} />
       <button className='search fa-brands fa-searchengin' />
     </Div>
   );

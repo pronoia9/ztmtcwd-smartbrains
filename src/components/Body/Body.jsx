a38bd4db47845df3d0aed1e79e6c0797c7901883
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-16 10:33:29 +0300

    adjust css classes

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -4,16 +4,16 @@ import ImageLinkForm from '../ImageLinkForm/ImageLinkForm';
 
 export default function Body() {
   return (
-    <Div ids={['body-content']} classNames={['container']}>
-      <Div classNames={['page-header', 'container', 'row justify-content-center', 'col-lg-10 col-md-8', 'cont text-center']}>
+    <Div classNames={['container']}>
+      <Div classNames={['page-header', 'row justify-content-center', 'col-lg-10 col-md-8', 'cont text-center']}>
         <h2 className='mb-10'>
           <span className='color-font'>Andrei</span> you are ranked <span className='color-font'>#5</span>
         </h2>
-        <Div classNames={['pt-30']}>
+        <Div classNames={['pt-50 pb-30']}>
           <ImageLinkForm />
         </Div>
       </Div>
-      <Div classNames={['blog-pg single section-padding pt-0', 'container', 'row justify-content-center', 'col-lg-6', 'post', 'img']}>
+      <Div classNames={['blog-pg single', 'container', 'row justify-content-center', 'col-lg-6', 'post', 'img']}>
         <img src='https://www.goodworklabs.com/wp-content/uploads/2020/06/Face-recognition-technology.png' alt='' />
       </Div>
     </Div>

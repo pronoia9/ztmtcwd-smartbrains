5e5b503b7c63fcf692997ff77c1876e315278645
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-17 06:07:35 -0400

    add placeholder as prop

--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import Div from '../General/Div';
 import './ImageLinkForm.css';
 
-export default function ImageLinkForm({ text = 'Placeholder' }) {
+export default function ImageLinkForm({ placeholder }) {
   return (
     <Div id={['image-link-form']} classNames={['image-link']}>
-      <input type='text' placeholder={text} />
+      <input type='text' placeholder={placeholder} />
       <button className='search fa-brands fa-searchengin' />
     </Div>
   );

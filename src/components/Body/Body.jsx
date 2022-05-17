0186ae5822014df8c187cc92f49fe6b50c44e8b5
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-17 11:53:46 -0400

    add imageURL prop

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -3,17 +3,17 @@ import Div from '../General/Div';
 import ImageLinkForm from '../ImageLinkForm/ImageLinkForm';
 import ImageFaceDetection from '../ImageFaceDetection/ImageFaceDetection';
 
-export default function Body({ name, rank, input, placeholder, inputChange, buttonClick, clear }) {
+export default function Body({ name, count, input, imageURL, placeholder, inputChange, buttonClick, clear }) {
   return (
     <Div classNames={['container']}>
       <Div classNames={['page-header', 'row justify-content-center', 'col-lg-10 col-md-8', 'cont text-center']}>
         <h2 className='mb-10'>
-          <span className='color-font'>{name}</span> you are ranked <span className='color-font'>#{rank}</span>.
+          <span className='color-font'>{name}</span> you have successfully detected <span className='color-font'>{count}</span> images.
         </h2>
         <p>This Magic Brain will detect faces in your pictures. Give it a try.</p>
         <ImageLinkForm input={input} placeholder={placeholder} inputChange={inputChange} buttonClick={buttonClick} clear={clear} />
       </Div>
-      <ImageFaceDetection input={input} />
+      <ImageFaceDetection imageURL={imageURL} />
     </Div>
   );
 }
--- src/components/ImageFaceDetection/ImageFaceDetection.jsx
+++ src/components/ImageFaceDetection/ImageFaceDetection.jsx
@@ -1,9 +1,9 @@
 import Div from '../General/Div';
 
-export default function ImageFaceDetection({ input }) {
+export default function ImageFaceDetection({ imageURL }) {
   return (
     <Div classNames={['blog-pg single', 'container', 'row justify-content-center', 'col-lg-6', 'post', 'img']}>
-      <img src={input} alt='' />
+      <img src={imageURL} alt='' />
     </Div>
   );
 }

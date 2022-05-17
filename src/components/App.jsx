a753ddd4be255c42cdc6831b7399510cfb956a72
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-17 11:55:10 -0400

    add imageURL state, fix setStates, etc

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,6 +8,7 @@ import Background from './Background/Background';
 import Navbar from './Navbar/Navbar';
 import Header from './Header/Header';
 import Body from './Body/Body';
+import react from 'react';
 // other / data
 const logo = require('../images/logo.png');
 const data = require('../data/data.json');
@@ -16,29 +17,25 @@ const keys = require('../data/keys.json');
 const app = new Clarifai.App({ apiKey: keys.clarifai });
 
 export default function App() {
-  const [state, setState] = useState({ user: null });
-  // SET TEMPORARY USER INFO
-  useEffect(() => setState({ user: { name: 'Andrei', count: 0, rank: 5, input: '' } }), []);
+  const [state, setState] = useState({ user: { name: 'Andrei', count: 0, rank: 0, imageURLs: [], imageURL: '', input: '' } });
+  // update count when an imageurl is added to the urls array
+  useEffect(() => setState((state) => ({user: {...state.user, count: state.user.imageURLs.length}})), [state.user.imageURLs])
 
   // Form Functions
-  const inputChange = (e) => {
-    setState((state) => ({ user: { ...state.user, input: e.target.value } }));
-  };
+  const inputChange = (e) => setState((state) => ({ user: { ...state.user, input: e.target.value } }));
   const buttonClick = () => {
+    setState((state) => ({ user: { ...state.user, imageURL: state.user.input } }));
+
     app.models.predict(Clarifai.FACE_DETECT_MODEL, state.user.input).then(
       (response) => {
-        // do something with response
-        console.log(response);
+        console.log(response); // do something with response
+        // if we got a positive response with the inputted imageURL, add it to the users search array and increase the search count
+        setState((state) => ({ user: { ...state.user, imageURLs: [state.user.imageURL] } }));
       },
-      (err) => {
-        // there was an error
-        console.err(err);
-      }
+      (err) => console.err(err)
     );
   };
-  const clear = () => {
-    setState((state) => ({ user: { ...state.user, input: '' } }));
-  };
+  const clear = () => setState((state) => ({ user: { ...state.user, input: '' } }));
 
   return (
     <div className='__next'>
@@ -47,7 +44,11 @@ export default function App() {
 
       <div className='particles circle-bg valign'>
         {/* header if not signed in, body if logged in */}
-        {!state.user ? <Header /> : <Body {...state.user} placeholder='Enter an image URL' inputChange={inputChange} buttonClick={buttonClick} clear={clear} />}
+        {!state.user ? (
+          <Header />
+        ) : (
+          <Body {...state.user} placeholder='Enter an image URL' inputChange={inputChange} buttonClick={buttonClick} clear={clear} />
+        )}
 
         <Background data={data.particles.vie} />
       </div>

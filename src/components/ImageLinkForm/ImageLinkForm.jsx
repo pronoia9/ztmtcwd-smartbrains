5083c7d337abb957ec899cafccccc92ea8e3a400
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-17 06:45:25 -0400

    change props

--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Div from '../General/Div';
 import './ImageLinkForm.css';
 
-export default function ImageLinkForm({ placeholder }) {
+export default function ImageLinkForm({ inputChange, placeholder, onClick }) {
   return (
     <Div id={['image-link-form']} classNames={['image-link']}>
-      <input type='text' placeholder={placeholder} />
-      <button className='search fa-brands fa-searchengin' />
+      <input type='text' placeholder={placeholder} onChange={(e) => inputChange(e)} />
+      <button className='search fa-brands fa-searchengin' onClick={onClick} />
     </Div>
   );
 }

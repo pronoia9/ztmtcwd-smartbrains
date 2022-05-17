9b8a6672e43b7b1b86d44ff88e92a5234224f7d3
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-17 06:07:26 -0400

    change props to state.user prop

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import Div from '../General/Div';
 import ImageLinkForm from '../ImageLinkForm/ImageLinkForm';
 
-export default function Body({ user = 'Andrei', rank = 5 }) {
+export default function Body({ user, placeholder }) {
   return (
     <Div classNames={['container']}>
       <Div classNames={['page-header', 'row justify-content-center', 'col-lg-10 col-md-8', 'cont text-center']}>
         <h2 className='mb-10'>
-          <span className='color-font'>{user}</span> you are ranked <span className='color-font'>#{rank}</span>.
+          <span className='color-font'>{user.name}</span> you are ranked <span className='color-font'>#{user.rank}</span>.
         </h2>
         <p>This Magic Brain will detect faces in your pictures. Give it a try.</p>
         <Div classNames={['pt-40 pb-20']}>
-          <ImageLinkForm />
+          <ImageLinkForm placeholder={placeholder} />
         </Div>
       </Div>
       <Div classNames={['blog-pg single', 'container', 'row justify-content-center', 'col-lg-6', 'post', 'img']}>

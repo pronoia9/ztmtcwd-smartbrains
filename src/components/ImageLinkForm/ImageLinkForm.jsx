871e39f7f9b3d038b9e3c9339f01474638bdf0d9
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-17 07:14:27 -0400

    add clear button (ongoing)

--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -6,6 +6,7 @@ export default function ImageLinkForm({ input, placeholder, inputChange, buttonC
   return (
     <Div id={['image-link-form']} classNames={['image-link']}>
       <input type='text' value={input} placeholder={placeholder} onChange={(e) => inputChange(e)} />
+      <button className={`clear${input.length > 0 && ' fa-solid fa-xmark'}`} onClick={buttonClick} />
       <button className='search fa-brands fa-searchengin' onClick={buttonClick} />
     </Div>
   );

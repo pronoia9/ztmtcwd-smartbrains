41a85113d4cba1fb60bdf33ac254d66c0faa7d8a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-17 07:42:16 -0400

    fix class name and add clear on click

--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import Div from '../General/Div';
 import './ImageLinkForm.scss';
 
-export default function ImageLinkForm({ input, placeholder, inputChange, buttonClick }) {
+export default function ImageLinkForm({ input, placeholder, inputChange, buttonClick, clear }) {
   return (
     <Div id={['image-link-form']} classNames={['image-link']}>
       <input type='text' value={input} placeholder={placeholder} onChange={(e) => inputChange(e)} />
-      <button className={`clear${input.length > 0 && ' fa-solid fa-xmark'}`} onClick={buttonClick} />
       <button className='search fa-brands fa-searchengin' onClick={buttonClick} />
+      <button className={`clear${input.length > 0 ? ' fa-solid fa-xmark' : ''}`} onClick={clear} />
     </Div>
   );
 }

d707e20c6db109381a84f786facbffb5c1b9d76b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-18 05:40:50 -0400

    change state to user

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,41 +15,44 @@ const data = require('../data/data.json');
 const keys = require('../data/keys.json');
 // Clarifai
 const app = new Clarifai.App({ apiKey: keys.clarifai });
+// TEMPORARY
+const Andrei = { name: 'Andrei', count: 0, rank: 0, history: [], imageURL: '', input: '', box: {} };
 
 export default function App() {
-  const temp = { user: { name: 'Andrei', count: 0, rank: 0, imageURLs: [], imageURL: '', input: '', box: {} } };
-  const [state, setState] = useState(temp);
+  const [user, setUser] = useState(Andrei);
 
   // Form Functions
-  const inputChange = (e) => setState((state) => ({ user: { ...state.user, input: e.target.value } }));
+  const inputChange = (e) => setUser((user) => ({ ...user, input: e.target.value }));
   const buttonClick = () => {
-    setState((state) => ({ user: { ...state.user, imageURL: state.user.input } }));
+    setUser((user) => ({ ...user, imageURL: user.input }));
 
     app.models
-      .predict(Clarifai.FACE_DETECT_MODEL, state.user.input)
+      .predict(Clarifai.FACE_DETECT_MODEL, user.input)
       .then((response) => calculateBox(response))
-      .then(() => (state.user.imageURLs[state.user.imageURLs.length - 1] !== state.user.input) && (setState((state) => ({ user: { ...state.user, count: state.user.count + 1, imageURLs: [...state.user.imageURLs, state.user.imageURL] } }))))
+      .then(() => user.history[user.history.length - 1] !== user.input && setUser((user) => ({ ...user, count: user.count + 1, history: [...user.history, user.imageURL] }))
+      )
       .catch((e) => console.log(e));
   };
-  const clear = () => setState((state) => ({ user: { ...state.user, input: '' } }));
+  const clear = () => setUser((user) => ({ ...user, input: '' }));
 
   const calculateBox = (data) => {
     const face = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('input-image');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height);
   };
 
   return (
     <Div ids={['app-container']} classNames={['__next']}>
       <Preloader />
-      <Navbar logo={logo} setState={setState} />
+      <Navbar logo={logo} setState={setUser} />
 
       <Div ids={['body-container']} classNames={['particles circle-bg valign']}>
         {/* header if not signed in, body if logged in */}
-        {!state.user ? (
-          <Header />
-        ) : (
-          <Body {...state.user} placeholder='Enter an image URL' inputChange={inputChange} buttonClick={buttonClick} clear={clear} />
-        )}
+        { !user ?
+          <Header /> :
+          <Body {...user} placeholder='Enter an image URL' inputChange={inputChange} buttonClick={buttonClick} clear={clear} /> }
 
         <Background data={data.particles.vie} />
       </Div>

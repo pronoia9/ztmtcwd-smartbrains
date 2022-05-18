3c5d6fa6a758e3c415dfdf2c58aef604cea78d6f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-18 15:56:20 +0300

    fix logo, add user as check to show signin/out

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import './Navbar.scss';
 
-export default function Navbar({ logo }) {
+export default function Navbar({ logo, user }) {
   return (
     <nav id='navigation' className='navbar navbar-expand-lg change'>
       <div className='container'>
-          <a className='logo' href='/'><img src={logo} alt='logo' /></a>
+          <a className='logo' href='/'><img src={logo.default} alt='logo' /></a>
         <div className='collapse navbar-collapse' id='navbarSupportedContent'>
           <ul className='navbar-nav ml-auto'>
             <li className='nav-item'><a className='nav-link' href='/'>Home</a></li>
-            <li className='nav-item'><a className='nav-link' href='/about'>About</a></li>
-            <li className='nav-item dropdown'>
+            {/* <li className='nav-item dropdown'>
               <span className='nav-link dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>Dropdown</span>
               <div className='dropdown-menu'><a className='dropdown-item' href='/blog/blog-dark/'>Dropdown Item</a></div>
-            </li>
-            <li className='nav-item'><a className='nav-link' href='/sign'>Sign In/Out</a></li>
+            </li> */}
+            <li className='nav-item'><a className='nav-link' href={`/sign${user ? 'out' : 'in'}`}>{ user ? 'Sign Out' : 'Sign In' }</a></li>
           </ul>
         </div>
       </div>

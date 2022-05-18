038973a59b36aa34c01d4149889f47e0ec6a54ac
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-18 15:53:13 +0300

    reorganize/format

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,13 +17,13 @@ const keys = require('../data/keys.json');
 // Clarifai
 const app = new Clarifai.App({ apiKey: keys.clarifai });
 // TEMPORARY
-const Andrei = { username: 'aneagoi', name: 'Andrei', email: 'andrei@gmail.com', password: 'ztm', count: 0, rank: 0, history: [], imageURL: '', input: '', boxes: [] };
-const users = [Andrei];
+const users = [], Andrei = { username: 'aneagoi', name: 'Andrei', email: 'andrei@gmail.com', password: 'ztm', count: 0, rank: 0, history: [], imageURL: '', input: '', boxes: [] };
+users.push(Andrei);
 
 export default function App() {
-  const [user, setUser] = useState(Andrei);
+  const [user, setUser] = useState(null);
 
-  // Form Functions
+  // Image Form Functions
   const inputChange = (e) => setUser((user) => ({ ...user, input: e.target.value }));
   const clear = () => setUser((user) => ({ ...user, input: '' }));
   const buttonClick = () => {
@@ -37,23 +37,25 @@ export default function App() {
     }
   };
 
-  // Clarifai / Box functions
+  // Clarifai / Box Functions
   const calculateBox = (data) => {
     const boxes = data.outputs[0].data.regions.map((elem) => elem.region_info.bounding_box);
     const image = document.getElementById('input-image'), width = Number(image.width), height = Number(image.height);
     return boxes.map((box => ({ left: box.left_col * width, top: box.top_row * height, right: width - (box.right_col * width), bottom: height - (box.bottom_row * height)})));
   };
   const displayBox = (box) => setUser((user) => ({ ...user, boxes: box }));
+
+  // Sign-in Functions
+  const verifyLogin = (email, pwd) => {}
   
   return (
     <Div ids={['app-container']} classNames={['__next']}>
       <Preloader />
-      <Navbar logo={logo} setState={setUser} />
+      <Navbar logo={logo} user={user} />
 
       <Div ids={['body-container']} classNames={['particles circle-bg valign']}>
         {/* header if not signed in, body if logged in */}
-        {/* {!user ? <Header /> : <Body {...user} inputChange={inputChange} buttonClick={buttonClick} clear={clear} />} */}
-        <Signin />
+        {!user ? <Header /> : <Body {...user} inputChange={inputChange} buttonClick={buttonClick} clear={clear} />}
 
         <Background data={data.particles.vie} />
       </Div>

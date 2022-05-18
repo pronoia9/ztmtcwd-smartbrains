ae2bd0b4dbfbe550d3c89235baf32a2fd623e6b8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-18 06:07:49 -0400

    add bounding box styles

--- src/components/ImageFaceDetection/ImageFaceDetection.jsx
+++ src/components/ImageFaceDetection/ImageFaceDetection.jsx
@@ -1,9 +1,11 @@
 import Div from '../General/Div';
+import './ImageFaceDetection.scss';
 
 export default function ImageFaceDetection({ imageURL, box }) {
   return (
     <Div classNames={['blog-pg single pb-60', 'container', 'row justify-content-center', 'col-lg-6', 'post', 'img']}>
       <img id='input-image' src={imageURL} alt='' />
+      {box && <div id='bounding-box' style={{ top: box.top, right: box.right, bottom: box.bottom, left: box.left }}></div>}
     </Div>
   );
 }

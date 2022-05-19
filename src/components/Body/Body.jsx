9b3d7bc129a0b468cd219cbbb174b31afff3b92e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-19 14:36:05 +0300

    re-do styles classes

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -5,15 +5,19 @@ import ImageFaceDetection from '../ImageFaceDetection/ImageFaceDetection';
 
 export default function Body({ name, count, input, imageURL, boxes, inputChange, buttonClick, clear }) {
   return (
-    <Div ids={['content-container']} classNames={['container']}>
-      <Div classNames={['page-header', 'row justify-content-center', 'col-lg-10 col-md-8', 'cont text-center']}>
-        <h2 className='mb-10'>
-          <span className='color-font'>{name}</span> you have successfully detected <span className='color-font'>{count}</span> images.
-        </h2>
-        <p>This Magic Brain will detect faces in your pictures. Give it a try.</p>
-        <ImageLinkForm input={input} inputChange={inputChange} buttonClick={buttonClick} clear={clear} />
+    <Div classNames={['image-detection section-padding position-re']}>
+      <Div classNames={['container']}>
+        <Div classNames={['page-header', 'row justify-content-center', 'col-lg-10 col-md-8', 'cont text-center']}>
+          <h2 className='mb-10'>
+            <span className='color-font'>{name}</span> you have successfully detected <span className='color-font'>{count}</span>{' '}
+            images.
+          </h2>
+          <p>This Magic Brain will detect faces in your pictures. Give it a try.</p>
+          <ImageLinkForm input={input} inputChange={inputChange} buttonClick={buttonClick} clear={clear} />
+        </Div>
+        <ImageFaceDetection imageURL={imageURL} boxes={boxes} />
       </Div>
-      <ImageFaceDetection imageURL={imageURL} boxes={boxes} />
+      <div className='line bottom right'></div>
     </Div>
   );
 }

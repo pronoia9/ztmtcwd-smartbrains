29d73504e55fd5e4b366c25035bf33fae9a3e83e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-19 12:38:56 +0300

    remove preloader

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import App from './components/App';
-import Preloader from './components/Preloader/Preloader';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <Preloader />
     <App />
   </BrowserRouter>
   // </React.StrictMode>

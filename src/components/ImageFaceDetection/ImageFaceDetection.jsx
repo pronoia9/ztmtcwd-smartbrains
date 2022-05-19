8f7a92468dda0ee36f0757a39c7a109f0d45d97d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-19 06:47:35 +0300

    remove unused style classes

--- src/components/ImageFaceDetection/ImageFaceDetection.jsx
+++ src/components/ImageFaceDetection/ImageFaceDetection.jsx
@@ -3,7 +3,7 @@ import './ImageFaceDetection.scss';
 
 export default function ImageFaceDetection({ imageURL, boxes }) {
   return (
-    <Div classNames={['blog-pg single pb-60', 'container', 'row justify-content-center', 'col-lg-6', 'post', 'img']}>
+    <Div ids={['image-face-detection ']} classNames={['container pb-60', 'row justify-content-center', 'col-lg-6', 'img']}>
       <img id='input-image' src={imageURL} alt='' />
       {boxes.map((box, idx) => (
         <div className='bounding-box' style={{ top: box.top, right: box.right, bottom: box.bottom, left: box.left }} key={idx}></div>

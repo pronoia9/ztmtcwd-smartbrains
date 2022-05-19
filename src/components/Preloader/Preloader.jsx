f0940fb4b015818f109e7245882fd9bd3bca858a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-19 12:39:12 +0300

    remove comments

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -51,20 +51,3 @@ const PaceFoo = () => {
     });
   }
 };
-
-// f = function () {
-//   Pace.on('start', function () {
-//     document.querySelector('#preloader').classList.remove('isdone'), document.querySelector('.loading').classList.remove('isdone');
-//   }),
-//     Pace.on('done', function () {
-//       document.querySelector('#preloader').classList.add('isdone'), document.querySelector('.loading').classList.add('isdone');
-//     }),
-//     document.querySelector('body').classList.contains('pace-done') &&
-//       (document.querySelector('#preloader').classList.add('isdone'), document.querySelector('.loading').classList.add('isdone')),
-//     window.addEventListener('load', function () {
-//       document.querySelector('#preloader').classList.add('isdone'),
-//         document.querySelector('.loading').classList.add('isdone'),
-//         document.querySelector('.pace-running.pace-running') &&
-//           document.querySelector('.pace-running.pace-running').classList.remove('pace-running');
-//     });
-// };

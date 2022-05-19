f93bfcf5e905d441f7d3f30b12385dc5bd071d95
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-19 14:36:53 +0300

    move particles circle-bg valign div, add circlesbg

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Div from '../General/Div';
+import CircleBg from '../Background/CircleBg';
 
 export default function Header() {
   return (
-    <Div ids={['header-container', null, null, null]} classNames={['container', 'row justify-content-center', 'col-lg-10', 'cont text-center']}>
-      <h1>
-        <span className='color-font'>SmartBrains</span> will detect faces in your <span className='color-font'>pictures</span>.
-      </h1>
+    <Div ids={['header-container']} classNames={['particles circle-bg valign']}>
+      <Div classNames={['container', 'row justify-content-center', 'col-lg-10', 'cont text-center']}>
+        <h1>
+          <span className='color-font'>SmartBrains</span> will detect faces in your <span className='color-font'>pictures</span>.
+        </h1>
+      </Div>
+      <CircleBg />
+      <div className='line bottom left'></div>
     </Div>
   );
 }

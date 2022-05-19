6f42a36fa6873ef82e1a38bb5e68279ccaeee8e2
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-19 15:13:23 +0300

    send signin function as prop

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -70,7 +70,7 @@ export default function App() {
     <Div ids={['app-container']} classNames={['__next']}>
       <Navbar logo={logo} user={user} signout={signout} />
       <Background data={data.particles.vie} />
-      <Routes user={user} inputChange={inputChange} buttonClick={buttonClick} clear={clear} />
+      <Routes user={user} inputChange={inputChange} buttonClick={buttonClick} clear={clear} signin={signin} />
     </Div>
   );
 }

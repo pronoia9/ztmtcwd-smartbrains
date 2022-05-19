82f6eae2a1198b0d357617d3e7e7996568d906dc
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-19 06:08:52 +0300

    change form to div, add logs for future funcs

--- src/components/Signin-out/Signin.jsx
+++ src/components/Signin-out/Signin.jsx
@@ -1,6 +1,6 @@
 import Div from '../General/Div';
 import FormGroup from './FormGroup';
-import './Signin.scss';
+import './Form.scss';
 
 export default function Signin() {
   return (
@@ -10,23 +10,17 @@ export default function Signin() {
         <h4 className='wow fadeInLeft'>Sign In</h4>
       </Div>
 
-      <Div classNames={['form md-mb50']}>
-        <form action='#' id='signin-form' className='form' onSubmit={(e) => e.preventDefault()}>
-          <Div classNames={['messages']}></Div>
+      <Div ids={[null, 'signin-form']} classNames={['form md-mb50', 'form']}>
+         <Div classNames={['messages']}></Div>
           <Div classNames={['controls']}>
             <FormGroup name='email' id='form_email' type='email' placeholder='Email' value='' />
             <FormGroup name='password' id='form_password' type='password' placeholder='Password' value='' />
           </Div>
-          <button type='submit' className='butn bord'>
-            <span>Sign In</span>
-          </button>
-        </form>
+          <button onClick={() => console.log('add sign in function')} className='butn bord'><span>Sign In</span></button>
       </Div>
 
       <Div classNames={['bottom-box pt-20']}>
-        <button>
-          <h6>Don't have an account?</h6>
-        </button>
+        <button onClick={() => console.log('add react router and change to register page')}><h6>Don't have an account?</h6></button>
       </Div>
     </Div>
   );

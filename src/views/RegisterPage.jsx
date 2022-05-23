0488db1d5e3f557189c5b9e3509ef499d752be00
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-23 14:25:11 +0300

    fix rendering a diff page on certain conditions

--- src/views/RegisterPage.jsx
+++ src/views/RegisterPage.jsx
@@ -1,7 +1,10 @@
 import { useNavigate } from 'react-router-dom';
 import Register from '../components/Forms/Register';
+import ProfilePage from './ProfilePage';
 
 export default function RegisterPage(props) {
   let navigate = useNavigate();
-  return props.state.user ? navigate(`/profile/${props.state.user.id}`) : <Register {...props} />;
+
+  if (props.state.user) { navigate(`/profile/${props.state.user.id}`); return <ProfilePage {...props} />; }
+  return <Register {...props} />;
 }
--- src/views/SigninPage.jsx
+++ src/views/SigninPage.jsx
@@ -1,7 +1,9 @@
 import { useNavigate } from 'react-router-dom';
 import Signin from '../components/Forms/Signin';
+import ProfilePage from './ProfilePage';
 
 export default function SigninPage(props) {
   let navigate = useNavigate();
-  return props.state.user ? navigate(`/profile/${props.state.user.id}`) : <Signin {...props} />;
+  if (props.state.user) { navigate(`/profile/${props.state.user.id}`); return <ProfilePage {...props} /> }
+  return <Signin {...props} />;
 }

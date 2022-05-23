63ff42a06dc4db3c36df9e8e09d5aa10cdb74f9b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-23 14:17:02 +0300

    use user info

--- src/components/FaceRecognition/FaceRecognition.jsx
+++ src/components/FaceRecognition/FaceRecognition.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import Div from '../General/Div';
-import ImageLinkForm from './ImageForm';
-import ImageFaceDetection from './Image';
+import ImageForm from './ImageForm';
+import Image from './Image';
 
-export default function ImageDetection({ user, input, imageURL, boxes, inputChange, buttonClick, clear }) {
+export default function ImageDetection({ state, setState, input, imageURL, boxes, inputChange, buttonClick, clear }) {
+  const user = state.user;
+
   return (
     <Div classNames={['face-recognition section-padding position-re mh-100vh']}>
       <Div classNames={['container']}>
         <Div classNames={['page-header', 'row justify-content-center', 'col-lg-10 col-md-8', 'cont text-center']}>
           <h2 className='mb-10'>
-            <span className='color-font'>{user.username}</span> you have successfully detected{' '}
-            <span className='color-font'>{user.entries}</span> images.
+            <span className='color-font'>{user.name ? user.name : user.username}</span> you have successfully detected <span className='color-font'>{user.entries}</span> images.
           </h2>
           <p>This Magic Brain will detect faces in your pictures. Give it a try.</p>
-          {/* <ImageLinkForm input={input} inputChange={inputChange} buttonClick={buttonClick} clear={clear} /> */}
+          {/* <ImageForm input={input} inputChange={inputChange} buttonClick={buttonClick} clear={clear} /> */}
         </Div>
-        {/* <ImageFaceDetection imageURL={imageURL} boxes={boxes} /> */}
+        {/* <Image imageURL={imageURL} boxes={boxes} /> */}
       </Div>
       <div className='line bottom right'></div>
     </Div>

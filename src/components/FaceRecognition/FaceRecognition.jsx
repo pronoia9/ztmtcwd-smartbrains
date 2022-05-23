84487845a4986c9702847af7a5b84d24e9fdd9a5
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-23 15:59:47 +0300

    adjust title/heading for users vs guests

--- src/components/FaceRecognition/FaceRecognition.jsx
+++ src/components/FaceRecognition/FaceRecognition.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Div from '../General/Div';
 import ImageForm from './ImageForm';
 import Image from './Image';
@@ -10,9 +11,15 @@ export default function ImageDetection({ state, inputChange, clear, buttonClick 
     <Div classNames={['face-recognition section-padding position-re mh-100vh']}>
       <Div classNames={['container']}>
         <Div classNames={['page-header', 'row justify-content-center', 'col-lg-10 col-md-8', 'cont text-center']}>
-          <h2 className='mb-10'>
-            <span className='color-font'>{user.name ? user.name : user.username}</span> you have successfully detected <span className='color-font'>{user.entries}</span> image{user.entries !== 1 && `s`}.
-          </h2>
+          {user ? (
+            <h2 className='mb-10'>
+              <span className='color-font'>{user.name ? user.name : user.username}</span> you have successfully detected <span className='color-font'>{user.entries}</span> image{user.entries !== 1 && `s`}.
+            </h2>
+          ) : (
+            <h2 className='mb-10'>
+              <span className='color-font'>Oops</span>, it looks like you're <span className='color-font'>not logged in</span>.
+            </h2>
+          )}
           <p>This Magic Brain will detect faces in your pictures. Give it a try.</p>
           <ImageForm input={state.input} inputChange={inputChange} clear={clear} buttonClick={buttonClick} />
         </Div>

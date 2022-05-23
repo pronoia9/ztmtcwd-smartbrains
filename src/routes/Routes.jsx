f2d3948a1d287c092bfa1d325b5cacb3966339cf
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-23 16:00:13 +0300

    add new page for visitors to routes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Routes, Route, useParams } from 'react-router-dom';
 // pages
 import HomePage from '../views/HomePage';
+import FaceRecognitionPage from '../views/FaceRecognitionPage';
 import ProfilePage from '../views/ProfilePage';
 import SigninPage from '../views/SigninPage';
 import RegisterPage from '../views/RegisterPage';
@@ -14,6 +15,7 @@ export default function App(props) {
     <Routes>
       <Route path='/register' element={<RegisterPage {...props} />} />
       <Route path='/signin' element={<SigninPage {...props} />} />
+      <Route path='/clarifai' element={<FaceRecognitionPage {...props} />} />
       <Route path='/profile/:userId' element={<ProfilePage {...props} />} />
       <Route path='/' element={<HomePage {...props} />} />
     </Routes>

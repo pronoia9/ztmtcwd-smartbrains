79bb6ddc99df0176606e31a278e4450e5c8dbfa1
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-23 13:38:40 +0300

    add ids to /profile page route

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useParams } from 'react-router-dom';
 // pages
 import HomePage from '../views/HomePage';
 import ProfilePage from '../views/ProfilePage';
@@ -8,11 +8,15 @@ import SigninPage from '../views/SigninPage';
 import RegisterPage from '../views/RegisterPage';
 
 export default function App(props) {
+  let { userId } = useParams();
+  
   return (
     <Routes>
       <Route path='/register' element={<RegisterPage {...props} />} />
       <Route path='/signin' element={<SigninPage {...props} />} />
-      <Route path='/profile' element={<ProfilePage {...props} />} />
+      <Route path='/profile'>
+        <Route path=':userId' element={<ProfilePage {...props} />} />
+      </Route>
       <Route path='/' element={<HomePage {...props} />} />
     </Routes>
   );

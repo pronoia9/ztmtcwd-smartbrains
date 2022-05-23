eef77c6688075485ecf3937fc62ce722fe82133d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-23 15:30:20 +0300

    adjust setstates with change to variable names

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,51 +17,45 @@ const app = new Clarifai.App({ apiKey: keys.clarifai });
 
 export default function App() {
   const [state, setState] = useState({ input: '', imageURL: '', boxes: [], user: null });
+  const user = state.user;
 
-  // Image Form Functions
-  const inputChange = (e) => {
-    // setState((user) => ({ ...user, input: e.target.value }));
-  };
-  const clear = () => {
-    // setState((user) => ({ ...user, input: '' }));
-  };
-  const buttonClick = () => {
-    // if (user.history[user.history.length - 1] !== user.input) {
-    //   setUser((user) => ({ ...user, imageURL: user.input, boxes: [] }));
-    //   app.models
-    //     .predict(Clarifai.FACE_DETECT_MODEL, user.input)
-    //     .then((response) => displayBox(calculateBox(response)))
-    //     .then(() => setUser((user) => ({ ...user, count: user.count + 1, history: [...user.history, user.imageURL] })))
-    //     .catch((err) => console.error(err));
-    // }
-  };
-
-  // Clarifai / Box Functions
-  const calculateBox = (data) => {
-    // const boxes = data.outputs[0].data.regions.map((elem) => elem.region_info.bounding_box);
-    // const image = document.getElementById('input-image'),
-    //   width = Number(image.width),
-    //   height = Number(image.height);
-    // return boxes.map((box) => ({
-    //   left: box.left_col * width,
-    //   top: box.top_row * height,
-    //   right: width - box.right_col * width,
-    //   bottom: height - box.bottom_row * height,
-    // }));
-  };
-  const displayBox = (box) => {
-    // setState((user) => ({ ...user, boxes: box }));
-  };
-
-  // Sign-in Functions
+  // User Functions
   const signout = () => setState(null);
   const loadUser = (data) => setState((state) => ({ ...state, user: data }));
+
+  // Image Form / Clarifai / Box Functions
+  const inputChange = (e) => setState((state) => ({ ...state, input: e.target.value }));
+  const clear = () => setState((state) => ({ ...state, input: '' }));
+  const buttonClick = () => {
+    // if the last searched image is not the current one
+    if (user.history[user.history.length - 1] !== state.input) {
+      setState((state) => ({ ...state, imageURL: state.input }));
+      app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, state.input)
+        .then((response) => displayBox(calculateBox(response)))
+        .then(() => setState((state) => ({...state, user: { ...user, entries: user.entries + 1, history: [...user.history, state.imageURL] }})))
+        .catch((err) => console.error(err));
+    }
+  };
+  const calculateBox = (data) => {
+    const boxes = data.outputs[0].data.regions.map((elem) => elem.region_info.bounding_box);
+    const image = document.getElementById('input-image'),
+      width = Number(image.width),
+      height = Number(image.height);
+    return boxes.map((box) => ({
+      left: box.left_col * width,
+      top: box.top_row * height,
+      right: width - box.right_col * width,
+      bottom: height - box.bottom_row * height,
+    }));
+  };
+  const displayBox = (box) => setState((state) => ({ ...state, boxes: box }));
 
   return (
     <Div ids={['app-container']} classNames={['__next']}>
       <Navbar logo={logo} user={state.user} signout={signout} />
       <Background data={data.particles.vie} />
-      <Routes state={state} useState={useState} loadUser={loadUser} inputChange={inputChange} buttonClick={buttonClick} clear={clear} />
+      <Routes state={state} setState={setState} loadUser={loadUser} inputChange={inputChange} clear={clear} buttonClick={buttonClick} />
     </Div>
   );
 }

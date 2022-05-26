8af6e8c6819006d8e4f0edb7f4f4b87e11674084
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-26 11:47:39 +0300

    remove facerecognition component

--- src/views/ProfilePage.jsx
+++ src/views/ProfilePage.jsx
@@ -1,11 +1,10 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import FaceRecognition from '../components/FaceRecognition/FaceRecognition';
 import HomePage from './HomePage';
 
 export default function ProfilePage(props) {
   let navigate = useNavigate();
   useEffect(() => { !props.state.user && navigate('/'); }, [props.state.user, navigate]);
 
-  return !props.state.user ? <HomePage {...props} /> : <FaceRecognition {...props} />;
+  return !props.state.user ? <HomePage {...props} /> : <></>;
 }

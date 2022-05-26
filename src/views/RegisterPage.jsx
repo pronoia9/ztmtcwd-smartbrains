a49bb270198e59f71943cde6edf7de800487eda3
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-26 12:32:37 +0300

    remove navigation if a users logged in

--- src/views/RegisterPage.jsx
+++ src/views/RegisterPage.jsx
@@ -4,8 +4,9 @@ import Register from '../components/Forms/Register';
 import ProfilePage from './ProfilePage';
 
 export default function RegisterPage(props) {
-  let navigate = useNavigate();
-  useEffect(() => { props.state.user && navigate(`/profile/${props.state.user.id}`); }, [props.state.user, navigate]);
+  // let navigate = useNavigate();
+  // useEffect(() => { props.state.user && navigate(`/clarifai/${props.state.user.id}`); }, [props.state.user, navigate]);
+  // return !props.state.user ? <Register {...props} /> : <ProfilePage {...props} />;
 
-  return !props.state.user ? <Register {...props} /> : <ProfilePage {...props} />;
+  return <Register {...props} />;
 }
--- src/views/SigninPage.jsx
+++ src/views/SigninPage.jsx
@@ -4,8 +4,9 @@ import Signin from '../components/Forms/Signin';
 import ProfilePage from './ProfilePage';
 
 export default function SigninPage(props) {
-  let navigate = useNavigate();
-  useEffect(() => { props.state.user && navigate(`/profile/${props.state.user.id}`); }, [props.state.user, navigate]);
-
-  return !props.state.user ? <Signin {...props} /> : <ProfilePage {...props} />;
+  // let navigate = useNavigate();
+  // useEffect(() => { props.state.user && navigate(`/clarifai/${props.state.user.id}`); }, [props.state.user, navigate]);
+  // return !props.state.user ? <Signin {...props} /> : <ProfilePage {...props} />;
+  
+  return <Signin {...props} />;
 }

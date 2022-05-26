f369ba780934e46c3660899193abe0f9503e381a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-26 12:26:05 +0300

    update signin function

--- src/components/Forms/Signin.jsx
+++ src/components/Forms/Signin.jsx
@@ -5,30 +5,35 @@ import FormGroup from './FormGroup';
 import './Form.scss';
 
 export default function Signin({ loadUser }) {
-  const empty = { username: '', password: '', messages: '' };
+  const empty = { email: '', password: '', messages: '' };
   const [user, setUser] = useState(empty);
   let navigate = useNavigate();
 
-  const signin = () => {
-    fetch('http://localhost:3000/signin', {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ ...user }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (typeof data === 'string') {
-          setUser((user) => ({ ...user, messages: data }));
-        } else {
+  async function signin() {
+    if (!user.email || !user.password) {
+      setUser((user) => ({ ...user, messages: 'Missing a field.' }));
+    } else {
+      try {
+        const init = { method: 'post', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ ...user }) };
+        const response = await fetch('http://localhost:3000/signin', init);
+        // console.log('response:', response);
+        if (response.status === 200) {
+          const data = await response.json();
+          // console.log('data:', data);
           setUser((user) => ({ ...user, messages: '' }));
           loadUser(data);
           setTimeout(() => {
             setUser(empty);
-            navigate(`/profile/${data.id}`);
+            navigate(`/clarifai`);
           }, 1000);
+        } else {
+          setUser((user) => ({ ...user, messages: 'There was an error.' }));
         }
-      });
-  };
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
 
   return (
     <Div ids={['signin-section']} classNames={['signin section-padding position-re mh-100vh']}>
@@ -45,12 +50,12 @@ export default function Signin({ loadUser }) {
           </Div>
           <Div classNames={['controls']}>
             <FormGroup
-              name='username'
-              id='form_username'
-              type='text'
-              placeholder='Username'
-              value={user.username}
-              onChange={(e) => setUser((user) => ({ ...user, username: e.target.value }))}
+              name='email'
+              id='email'
+              type='email'
+              placeholder='Email'
+              value={user.email}
+              onChange={(e) => setUser((user) => ({ ...user, email: e.target.value }))}
             />
             <FormGroup
               name='password'

417e090ff00f59a248e9dac3f4d4a094409e2ae8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-01 15:46:15 +0300

    add toggle function for input disable

--- src/components/Profile/ItemBox.jsx
+++ src/components/Profile/ItemBox.jsx
@@ -1,14 +1,15 @@
-import Div from "../General/Div";
+import Div from '../General/Div';
+import FormGroup from '../Forms/FormGroup';
 
-export default function ItemBox() {
+export default function ItemBox(props) {
   return (
     <Div classNames={['col-6 wow fadeInLeft', 'item-box no-curve']}>
-      <Div>
-        <span className='icon color-font pe-7s-config'></span>
+      <Div classNames={['valign']}>
+        <span className={`icon color-font ${props.icon ? 'pe-7s-config' : 'pe-7s-like2'}`} onClick={props.disable}></span>
       </Div>
       <Div classNames={['cont']}>
-        <h6>Name</h6>
-        <p>Name</p>
+        <h6>{props.placeholder}</h6>
+        <FormGroup {...props} />
       </Div>
     </Div>
   );

7bc7387e54d02e69f4ccae336b546336e70d331e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-01 17:03:43 +0300

    enable conditions and navigation

--- src/views/ProfilePage.jsx
+++ src/views/ProfilePage.jsx
@@ -4,9 +4,8 @@ import HomePage from './HomePage';
 import Profile from '../components/Profile/Profile';
 
 export default function ProfilePage(props) {
-  // let navigate = useNavigate();
-  // useEffect(() => { !props.state.user && navigate('/'); }, [props.state.user, navigate]);
+  let navigate = useNavigate();
+  useEffect(() => { !props.state.user && navigate('/'); }, [props.state.user, navigate]);
 
-  // return !props.state.user ? <HomePage {...props} /> : <Profile {...props}/>;
-  return <Profile {...props}/>;
+  return !props.state.user ? <HomePage {...props} /> : <Profile {...props}/>;
 }

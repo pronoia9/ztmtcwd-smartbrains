169974e2e574d22a016b577b54c0651367a80aca
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-01 14:42:26 +0300

    add initial profile page component

--- src/views/ProfilePage.jsx
+++ src/views/ProfilePage.jsx
@@ -1,10 +1,12 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import HomePage from './HomePage';
+import Profile from '../components/Profile/Profile';
 
 export default function ProfilePage(props) {
-  let navigate = useNavigate();
-  useEffect(() => { !props.state.user && navigate('/'); }, [props.state.user, navigate]);
+  // let navigate = useNavigate();
+  // useEffect(() => { !props.state.user && navigate('/'); }, [props.state.user, navigate]);
 
-  return !props.state.user ? <HomePage {...props} /> : <></>;
+  // return !props.state.user ? <HomePage {...props} /> : <Profile {...props}/>;
+  return <Profile {...props}/>;
 }

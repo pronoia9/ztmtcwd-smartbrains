41c5f36972fd452286851963e963be63674dae77
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-03 13:15:24 +0300

    spread props, change function on icon click

--- src/components/Profile/ItemBox.jsx
+++ src/components/Profile/ItemBox.jsx
@@ -1,14 +1,14 @@
 import Div from '../General/Div';
 import FormGroup from '../Forms/FormGroup';
 
-export default function ItemBox(props) {
+export default function ItemBox({ icon, iconBtn, placeholder, ...props }) {
   return (
     <Div classNames={['col-6 wow fadeInLeft', 'setting-box no-curve']}>
       <Div classNames={['valign']}>
-        <span className={`icon color-font ${props.icon ? 'pe-7s-config' : 'pe-7s-like2'}`} onClick={props.disable}></span>
+        <span className={`icon color-font ${icon ? 'pe-7s-config' : 'pe-7s-like2'}`} onClick={iconBtn}></span>
       </Div>
       <Div classNames={['cont']}>
-        <h6>{props.placeholder}</h6>
+        <h6>{placeholder}</h6>
         <FormGroup {...props} />
       </Div>
     </Div>

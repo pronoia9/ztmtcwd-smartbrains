947c8261551fe64df217c12ac430ab38fb255f58
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-03 12:13:03 +0300

    rename classes

--- src/components/Profile/ItemBox.jsx
+++ src/components/Profile/ItemBox.jsx
@@ -3,7 +3,7 @@ import FormGroup from '../Forms/FormGroup';
 
 export default function ItemBox(props) {
   return (
-    <Div classNames={['col-6 wow fadeInLeft', 'item-box no-curve']}>
+    <Div classNames={['col-6 wow fadeInLeft', 'setting-box no-curve']}>
       <Div classNames={['valign']}>
         <span className={`icon color-font ${props.icon ? 'pe-7s-config' : 'pe-7s-like2'}`} onClick={props.disable}></span>
       </Div>

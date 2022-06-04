482006149a4f979c0397d4c18fee3f854e7d8e6f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-04 11:34:55 +0300

    change useNavigate() to <Navigate />

--- src/views/RegisterPage.jsx
+++ src/views/RegisterPage.jsx
@@ -1,11 +1,6 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import Register from '../components/Forms/Register';
-import ProfilePage from './ProfilePage';
 
 export default function RegisterPage(props) {
-  let navigate = useNavigate();
-  useEffect(() => { props.state.user && navigate(`/clarifai/${props.state.user.id}`); }, [props.state.user, navigate]);
-  
-  return !props.state.user ? <Register {...props} /> : <ProfilePage {...props} />;
+  return props.state.user ? <Navigate to='/' /> : <Register {...props} />;
 }
--- src/views/SigninPage.jsx
+++ src/views/SigninPage.jsx
@@ -1,11 +1,6 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import Signin from '../components/Forms/Signin';
-import ProfilePage from './ProfilePage';
 
 export default function SigninPage(props) {
-  let navigate = useNavigate();
-  useEffect(() => { props.state.user && navigate(`/clarifai/${props.state.user.id}`); }, [props.state.user, navigate]);
-  
-  return !props.state.user ? <Signin {...props} /> : <ProfilePage {...props} />;
+  return props.state.user ? <Navigate to='/' /> : <Signin {...props} />;
 }

dd8e19eb3ecef7a30af950ca97a170d4caeb0f9a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-04 12:00:33 +0300

    add <Navigate />s under conditions

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import { Routes, Route, useParams } from 'react-router-dom';
+import { Routes, Route, useParams, Navigate } from 'react-router-dom';
 // pages
 import HomePage from '../views/HomePage';
 import FaceRecognitionPage from '../views/FaceRecognitionPage';
@@ -9,16 +9,16 @@ import SigninPage from '../views/SigninPage';
 import RegisterPage from '../views/RegisterPage';
 
 export default function App(props) {
-  let { userId } = useParams();
-  
+  const user = props.state.user;
+
   return (
     <Routes>
-      <Route path='/register' element={<RegisterPage {...props} />} />
-      <Route path='/signin' element={<SigninPage {...props} />} />
-      <Route path='/clarifai/:userId' element={<FaceRecognitionPage {...props} />} />
-      <Route path='/clarifai' element={<FaceRecognitionPage {...props} />} />
-      <Route path='/profile/:userId' element={<ProfilePage {...props} />} />
-      <Route path='/' element={<HomePage {...props} />} />
+      <Route path='/register'         element={user ? <Navigate to={`/clarifai/${user.id}`} /> : <RegisterPage {...props} />} />
+      <Route path='/signin'           element={user ? <Navigate to={`/clarifai/${user.id}`} /> : <SigninPage {...props} />} />
+      <Route path='/clarifai'         element={<FaceRecognitionPage {...props} />} />
+      <Route path='/clarifai/:userId' element={user ? <FaceRecognitionPage {...props} /> : <Navigate to='/clarifai' />} />
+      <Route path='/profile/:userId'  element={user ? <ProfilePage {...props} /> : <Navigate to={'/'} />}></Route>
+      <Route path='/'                 element={<HomePage />} />
     </Routes>
   );
 }

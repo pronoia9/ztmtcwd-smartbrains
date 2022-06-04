0902884607434913772e50d17a882ea1c2aa573e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-04 12:11:22 +0300

    add navigation to gallery

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -13,12 +13,14 @@ export default function App(props) {
 
   return (
     <Routes>
-      <Route path='/register'         element={user ? <Navigate to={`/clarifai/${user.id}`} /> : <RegisterPage {...props} />} />
-      <Route path='/signin'           element={user ? <Navigate to={`/clarifai/${user.id}`} /> : <SigninPage {...props} />} />
-      <Route path='/clarifai'         element={<FaceRecognitionPage {...props} />} />
+      <Route path='/register' element={user ? <Navigate to={`/clarifai/${user.id}`} /> : <RegisterPage {...props} />} />
+      <Route path='/signin' element={user ? <Navigate to={`/clarifai/${user.id}`} /> : <SigninPage {...props} />} />
+      <Route path='/clarifai' element={<FaceRecognitionPage {...props} />} />
       <Route path='/clarifai/:userId' element={user ? <FaceRecognitionPage {...props} /> : <Navigate to='/clarifai' />} />
-      <Route path='/profile/:userId'  element={user ? <ProfilePage {...props} /> : <Navigate to={'/'} />}></Route>
-      <Route path='/'                 element={<HomePage />} />
+      <Route path='/profile/:userId' element={user ? <ProfilePage {...props} /> : <Navigate to={'/'} />}></Route>
+      {/* Not implemented yet. Have to implement a search history save in the database first. */}
+      <Route path='/profile/:userId/gallery' element={user ? <Navigate to={`/profile/${user.id}`} /> : <Navigate to='/' />}></Route>
+      <Route path='/' element={<HomePage />} />
     </Routes>
   );
 }

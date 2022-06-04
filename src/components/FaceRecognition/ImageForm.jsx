d10606ad50f4e8258bdc980015fb76682556a28a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-04 18:45:44 +0300

    fix id -> ids

--- src/components/FaceRecognition/ImageForm.jsx
+++ src/components/FaceRecognition/ImageForm.jsx
@@ -4,7 +4,7 @@ import './ImageForm.scss';
 
 export default function ImageForm({ input, inputChange, buttonClick, clear }) {
   return (
-    <Div id={['form-container', 'image-link-form']} classNames={['pt-20', 'image-link']}>
+    <Div ids={['image-link-form']} classNames={['pt-20', 'image-link']}>
       <input type='text' value={input} placeholder='Enter an image URL' onChange={(e) => inputChange(e)} />
       <button className='search fa-brands fa-searchengin' onClick={buttonClick} />
       <button className={`clear${input.length > 0 ? ' fa-solid fa-xmark' : ''}`} onClick={clear} />

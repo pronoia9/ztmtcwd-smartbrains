2ab9406743f0873fcf33f165ec9a061bef73c293
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-05 12:43:10 +0300

    rename id -> header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import CircleBg from '../Background/CircleBg';
 
 export default function Header() {
   return (
-    <Div ids={['header-container']} classNames={['particles circle-bg valign']}>
+    <Div ids={['header']} classNames={['particles circle-bg valign']}>
       <Div classNames={['container', 'row justify-content-center', 'col-lg-10', 'cont text-center']}>
         <h1>
           <span className='color-font'>SmartBrains</span> will detect faces in your <span className='color-font'>pictures</span>.

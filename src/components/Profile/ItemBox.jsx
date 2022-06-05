41d3a00a41e311072e762cdcdc62659b6f7b9c3d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-05 12:39:56 +0300

    remove animation classes

--- src/components/Profile/ItemBox.jsx
+++ src/components/Profile/ItemBox.jsx
@@ -3,7 +3,7 @@ import FormGroup from '../Forms/FormGroup';
 
 export default function ItemBox({ icon, iconBtn, placeholder, ...props }) {
   return (
-    <Div classNames={['col-6 wow fadeInLeft', 'setting-box no-curve']}>
+    <Div classNames={['col-6', 'setting-box no-curve']}>
       <Div classNames={['valign']}>
         <span className={`icon color-font ${icon ? 'pe-7s-config' : 'pe-7s-like2'}`} onClick={iconBtn}></span>
       </Div>

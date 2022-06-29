2bf79595a288e221e6d9fab219f770bf7f1c7d38
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-29 14:42:49 -0400

    send url as prop to routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -78,6 +78,7 @@ export default function App() {
         clear={clear}
         clearImages={clearImages}
         buttonClick={buttonClick}
+        secret={secret}
       />
       <Footer logo={logo} />
     </Div>
